fdc9fee6fb31d03977d4321dabd33ba0ce5055d9
JurreWeijer <j.a.j.weijer@lumc.nl>
2026-07-30 13:22:40 +0200

    Remove experimental prototype retrieval

--- src/pathforge/slide_retrieval/representation_strategies/strategies/__init__.py
+++ src/pathforge/slide_retrieval/representation_strategies/strategies/__init__.py
@@ -1,9 +1,6 @@
 from pathforge.slide_retrieval.representation_strategies.strategies.hshr_features import (
     HSHRFeatures,
 )
-from pathforge.slide_retrieval.representation_strategies.strategies.pbms import (
-    PBMSFeatures,
-)
 from pathforge.slide_retrieval.representation_strategies.strategies.sdm_features import (
     SDMFeatures,
 )
@@ -19,7 +16,6 @@ from pathforge.slide_retrieval.representation_strategies.strategies.sish_rgb imp
 
 __all__ = [
     "HSHRFeatures",
-    "PBMSFeatures",
     "SDMFeatures",
     "SPLICEFeatures",
     "SPLICERGB",
